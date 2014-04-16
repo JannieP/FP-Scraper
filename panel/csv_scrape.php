7813dd22cd068ddb8218f5bba3c21bd62a09fe10
JannieP <jannie@pieterse.me>
2014-04-16 13:12:28 +0200

    Update csv_scrape.php
    Added title function

--- panel/csv_scrape.php
+++ panel/csv_scrape.php
@@ -1,1 +1,14 @@
-<?php ?>
+<?php 
+
+
+function csvScrapeFormTitle(){
+  print('  
+      <div class="wrap" id="fps_options_page">
+      <div id="icon-options-general" class="icon32"><br /></div>
+      <h2>Wordpress FocalPrice Scraper to CSV Options</h2>
+      <span style="font-size: medium">This pluggin will scrape the listing and the each item and popolate a fully featured CSV file</span>
+    ');
+}
+
+
+?>
